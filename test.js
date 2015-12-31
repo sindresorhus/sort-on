f4245c1cbbcf6a6bb9c408978aa023bd939d2352
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:37:09 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,112 +1,91 @@
-'use strict';
-var test = require('ava');
-var sortOn = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
-	t.assert(sortOn([
+test(t => {
+	t.is(fn([
 		{foo: 'b'},
 		{foo: 'a'},
 		{foo: 'c'}
-	], 'foo')[0].foo === 'a');
+	], 'foo')[0].foo, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 2},
 		{foo: 1},
 		{foo: 3}
-	], 'foo')[0].foo === 1);
+	], 'foo')[0].foo, 1);
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 'b', bar: 'b'},
 		{foo: 'a', bar: 'b'},
 		{foo: 'a', bar: 'a'},
 		{foo: 'c', bar: 'c'}
-	], ['foo', 'bar'])[0].bar === 'a');
+	], ['foo', 'bar'])[0].bar, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: {bar: 'b'}},
 		{foo: {bar: 'a'}},
 		{foo: {bar: 'c'}}
-	], 'foo.bar')[0].foo.bar === 'a');
+	], 'foo.bar')[0].foo.bar, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 'b'},
 		{foo: 'a'},
 		{foo: 'c'}
-	], function (prop) {
-		return prop.foo;
-	})[0].foo === 'a');
+	], prop => prop.foo)[0].foo, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 'b', bar: 'b'},
 		{foo: 'a', bar: 'b'},
 		{foo: 'a', bar: 'a'},
 		{foo: 'c', bar: 'c'}
 	], [
-		function (prop) {
-			return prop.foo;
-		},
-		function (prop) {
-			return prop.bar;
-		}
-	])[0].bar === 'a');
+		prop => prop.foo,
+		prop => prop.bar
+	])[0].bar, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 'b', bar: 'a'},
 		{foo: 'a', bar: 'b'},
 		{foo: 'a', bar: 'c'},
 		{foo: 'c', bar: 'c'}
 	], [
-		function (prop) {
-			return prop.foo;
-		},
-		function (prop) {
-			return prop.bar;
-		}
-	])[0].bar === 'b');
+		prop => prop.foo,
+		prop => prop.bar
+	])[0].bar, 'b');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 2, bar: 2},
 		{foo: 1, bar: 2},
 		{foo: 1, bar: 1},
 		{foo: 3, bar: 3}
 	], [
-		function (prop) {
-			return prop.foo;
-		},
-		function (prop) {
-			return prop.bar;
-		}
-	])[0].bar === 1);
+		prop => prop.foo,
+		prop => prop.bar
+	])[0].bar, 1);
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 2, bar: 1},
 		{foo: 1, bar: 2},
 		{foo: 1, bar: 3},
 		{foo: 3, bar: 3}
 	], [
-		function (prop) {
-			return prop.foo;
-		},
-		function (prop) {
-			return prop.bar;
-		}
-	])[0].bar === 2);
+		prop => prop.foo,
+		prop => prop.bar
+	])[0].bar, 2);
 
-	var sorted = sortOn([
+	const sorted = fn([
 		{bar: 'b'},
 		{foo: 'b'},
 		{foo: 'a'}
 	], 'foo');
 
 	// {bar: 'b'} is not sorted so it might be the first or the second element
-	t.assert(sorted[0].foo || sorted[1].foo === 'a');
+	t.is(sorted[0].foo || sorted[1].foo, 'a');
 
-	t.assert(sortOn([
+	t.is(fn([
 		{foo: 'b', bar: 'a'},
 		{foo: 'a', bar: 'c'},
 		{foo: 'a', bar: 'b'},
 		{foo: 'c', bar: 'c'}
-	], ['foo', 'bar'])[0].bar === 'b');
-
-	t.end();
+	], ['foo', 'bar'])[0].bar, 'b');
 });
