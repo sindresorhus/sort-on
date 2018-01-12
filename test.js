6e4f686ec90fcbfb334aa3a9743c436f66f874fa
Ricardo-Silva91 <ricardos897@hotmail.com>
2018-01-12 02:27:01 +0000

    tests added

--- test.js
+++ test.js
@@ -125,4 +125,16 @@ test(t => {
 		{foo: null},
 		{foo: 'b'}
 	], '-foo')[0].foo, null);
+
+	t.is(m([
+		{foo: 'a'},
+		{foo: ''},
+		{foo: 'b'}
+	], 'foo')[2].foo, '');
+
+	t.is(m([
+		{foo: 'a'},
+		{foo: ''},
+		{foo: 'b'}
+	], '-foo')[0].foo, '');
 });
