2ca51604a2499b06c2924564af7dc30b53134098
Ricardo-Silva91 <ricardos897@hotmail.com>
2018-01-12 01:38:18 +0000

    fix for #14 - After sorting, empty strings are at the beginning of the sorted array

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ module.exports = (arr, prop) => {
 				return false;
 			}
 
-			if (y === null) {
+			if (y === null || y === '') {
 				ret = desc ? 1 : -1;
 				return true;
 			}
 
-			if (x === null) {
+			if (x === null  || x === '') {
 				ret = desc ? -1 : 1;
 				return true;
 			}
